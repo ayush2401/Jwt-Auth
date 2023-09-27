557958e6c36cd68a0b781aa43b82443a7e4959a2
ayushsaraf <ayushsaraf.saraf15@gmail.com>
2023-09-28 01:34:46 +0530

    data

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+const ok = 1
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
